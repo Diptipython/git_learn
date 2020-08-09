26586c3b7628e041c640edc2bca563fd3d733a07
Dipti.Deshpande <diptiad1gmail.com>
2020-08-09 20:17:28 +0200

    Committed demo.java to local repo

---
+++ demo.java
@@ -0,0 +1,16 @@
+public class whileclass1 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	
+			 				int i = 1;
+			
+					 						
+					do {
+			 																	System.out.println("Value of i =" +i);
+			 																				i ++;
+			 																						}
+			 																								
+																				 																														while (i <= 10) ;
+					// 																					
+
