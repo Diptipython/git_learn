7c6b10edb7f2f3a045cf923cf45920542fb222ff
Dipti.Deshpande <diptiad1gmail.com>
2020-08-12 00:13:52 +0200

    Modified whileclass1

--- whileclass1.java
+++ whileclass1.java
@@ -3,12 +3,15 @@
 	 public static void main(String[] args) {
 
 		 int i = 1;
+		 int j = 0;
 		 do {
 			 System.out.println("i=" +i);
+                          
 			 i++;
 		 }
 
 		 while (i < = 10);
+		 System.out.println("j="+j);
 	 }
  }
 
