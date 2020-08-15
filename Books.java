4a818266c1cd3859ca96e0de7c4676c84954325b
Diptipython <49412417+Diptipython@users.noreply.github.com>
2020-08-15 17:18:48 +0200

    Update to Books.java
    1st Update to Books.java

--- Books.java
+++ Books.java
@@ -3,12 +3,12 @@ public class Books {
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 	
-			 				int i = 1;
-			        int a = 2;					
-					if {
+			 	int i = 1;
+			        					
+	 if {
           i < 5 ;
           System.out.println("Book =" +i);
-			 		i ++;
+	i ++;
 			 																						}
 			 																								
 																				 																														
