90fbf32649f818b3e42720ac9ac70b783ae07626
Diptipython <49412417+Diptipython@users.noreply.github.com>
2020-08-15 17:13:54 +0200

    Create Books.java
    Created new  Books file

---
+++ Books.java
@@ -0,0 +1,16 @@
+public class Books {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	
+			 				int i = 1;
+			        int a = 2;					
+					if {
+          i < 5 ;
+          System.out.println("Book =" +i);
+			 		i ++;
+			 																						}
+			 																								
+																				 																														
+					// 																					
+
